3ae0023259d226779a69056cd005f549468ec068
Karem de Souza <krmcristine@gmail.com>
2020-12-25 22:32:32 -0300

    Pokedex Vue

--- README.md
+++ README.md
@@ -1,10 +1,29 @@
-# pokedex
+# Pokedex
 
+## Description
+```
+The application presents catalog of first generation pokemon, it is possible to change the avatar sprite and still search for Pokénmons.
+```
 ## Project setup
 ```
-npm install
+For the development of the application the bulma css framework and the Axios HTTP client were used
 ```
+## installing the bulma framework
+```
+npm install bulma
 
+```
+## Read More in 
+
+[bulma](https://bulma.io/)
+
+
+## installing o axios
+```
+npm install axios
+```
+## Read More in 
+[axios - npm]( https://www.npmjs.com/package/axios)
 ### Compiles and hot-reloads for development
 ```
 npm run serve
--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,64 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <div class="column is-half is-offset-one-quarter">
+    <img src="./assets/pokemon.png" alt="">
+    <hr>
+    <h4 class="is-size-4">POKEDEX</h4>
+    <input class="input is-rounded" type="text"  placeholder="Buscar pkémon pelo nome" v-model="busca">
+   <button class="button is-medium is-fullwidth is-success" id="btn" @click="buscar()">Buscar</button>
+    <div v-for="(poke,index) in filteredPokemons" :key="poke.url">
+    <pokemon :name="poke.name" :url="poke.url" :num="index+1" />
+  </div>
+    
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+import pokemon from "./components/pokemon"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+   pokemon
+  },
+  data(){
+    return{
+      pokemons:[],
+      filteredPokemons:[],
+      busca:''
+      }
+  },
+   computed:{
+  //   resultadoBusca: function(){
+  //     if(this.busca == '' || this.busca == ' '){
+  //       return this.pokemons
+  //   }else{
+  //     return this.pokemons.filter(pokemon => pokemon.name == this.busca)
+  //   }
+  // }
+  },
+  created(){
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then(res => {
+      console.log("Pegou a Lista de Pkemóns")
+      this.pokemons = res.data.results
+      this.filteredPokemons = res.data.results
+
+    })
+  },
+  methods:{
+    buscar(){
+      this.filteredPokemons = this.pokemons
+          if(this.busca == '' || this.busca == ' '){
+            this.filteredPokemons = this.pokemons;
+          }else{
+            this.filteredPokemons = this.pokemons.filter(pokemon => pokemon.name == this.busca)
+          }
+          console.log("Buscar")
+      
+    }
   }
 }
 </script>
@@ -24,5 +71,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#btn {
+  margin-top: 3%;
 }
 </style>
